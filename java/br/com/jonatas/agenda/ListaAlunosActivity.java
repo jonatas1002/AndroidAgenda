c59bdde225ff6eb997972ae01588f0c6dfbacf85
Jonatas Stein Ribeiro II <jonatas1002@gmail.com>
2019-02-02 16:02:42 -0300

    Curso Android II. Aula 1 onde foram adicionados os menus de contexto e funcionalidades de ligar, mandar sms, visitar site e ver no mapa.

--- java/br/com/jonatas/agenda/ListaAlunosActivity.java
+++ java/br/com/jonatas/agenda/ListaAlunosActivity.java
@@ -1,6 +1,11 @@
 package br.com.jonatas.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Browser;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -48,6 +53,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
             }
         });
 
+        //Informa ao Android a existencia do menu de contexto...
         registerForContextMenu(listaAlunos);
 
     }
@@ -69,13 +75,53 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        //Adiciona os menus de contexto na activity lista alunos.
+        MenuItem itemSite = menu.add("Visitar site");
         MenuItem deletar = menu.add("Deletar");
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        MenuItem itemMapa = menu.add("Visualizar no mapa");
+        MenuItem itemLigar = menu.add("Ligar");
+
+
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:"+ aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+        String site = aluno.getEmail();
+        if(!site.startsWith("http://")){
+            site = "http://" + site;
+        }
+        intentSite.setData(Uri.parse(site));
+        itemSite.setIntent(intentSite);
+
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 123);
+                }else {
+
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+
+            }
+        });
+
+        //Metodo que dá o comportamento ao menu de contexto deletar
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
 
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
                 dao.deleta(aluno);
